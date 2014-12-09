13d10e7d27f9cafcb3e37df5f1d2250b66dd0b99
zhangpu <zhangpu.2000@gmail.com>
2014-12-10 01:24:28 +0800

    get proxy ip via resolve

--- proxy.pac.js
+++ proxy.pac.js
@@ -1,7 +1,7 @@
 function FindProxyForURL(url, host) {
   var direct = 'DIRECT';
-  var http_proxy = 'PROXY 192.168.1.3:8080';
-  var socks_proxy = 'SOCKS5 192.168.1.106:1080; SOCKS 192.168.1.106:1080'
+  var http_proxy = 'PROXY ' + dnsResolve('http-server') + ':8080';
+  var socks_proxy = 'SOCKS5 ' + dnsResolve('socks-server') + ':1080';
   var blocked_list = [
     ///////////////////////
     // self inspired list
